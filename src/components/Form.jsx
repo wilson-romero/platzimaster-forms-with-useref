65b52761b72750f1ce45b7760ccdaabc6c782fad
Wilson Romero <wilson.romero@gmail.com>
2020-03-28 10:57:29 -0500

    Form using useRef

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+/* eslint-disable no-param-reassign */
+import React, { useRef } from 'react';
 
 export default function Form() {
+  const formRef = useRef();
+
+  function resetValues({ current }) {
+    const form = current;
+    Array.from(form).map(el => {
+      if (el.type.match('text|email|tel')) el.value = '';
+      return 0;
+    });
+  }
+
+  function save(form) {
+    const data = Array.from(form).reduce(
+      (obj, [key, value]) => ({ ...obj, [key]: value }),
+      {}
+    );
+    console.log(data);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const form = new FormData(formRef.current);
+    save(form);
+    resetValues(formRef);
+  }
+
   return (
-    <form>
-      <h2>Example</h2>
-      <input type="text" name="first-name" placeholder="First name" />
-      <input type="text" name="last-name" placeholder="Last name" />
+    <form ref={formRef} onSubmit={handleSubmit}>
+      <h2>Example with useRef()</h2>
+      <input type="text" name="firstname" placeholder="First name" />
+      <input type="text" name="lastname" placeholder="Last name" />
       <input type="email" name="email" placeholder="Email" />
       <input type="tel" name="phone" placeholder="Phone number" />
       <button type="submit">Submit</button>
